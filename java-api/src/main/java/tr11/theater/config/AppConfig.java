9bae484af649ccee061aafdfe0af8ecc2001d974
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-11-03 15:21:46 +0200

    beck up

--- java-api/src/main/java/tr11/theater/config/AppConfig.java
+++ java-api/src/main/java/tr11/theater/config/AppConfig.java
@@ -4,21 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import tr11.theater.security.intercaptor.RateLimitInterceptor;
 
 @RequiredArgsConstructor
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-    private final RateLimitInterceptor interceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor)
-                .addPathPatterns("/prizes/by-actor");
-    }
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
